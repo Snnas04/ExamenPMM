0c93f81c9cc5980023a32a1bdb871cd2d21160ea
Marc <marcsans26@gmail.com>
2024-02-15 17:03:20 +0100

    intentar obrir detalls d'abre, no funciona

--- examen_final_sans/lib/screen/info_abres_screen.dart
+++ examen_final_sans/lib/screen/info_abres_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoAbres extends StatelessWidget {
   final dynamic abre;
 
   InfoAbres({Key? key, required this.abre}) : super(key: key);
+
+  final detall = 'https://examen-practic-sim-default-rtdb.europe-west1.firebasedatabase.app/arbres.json';
+
+    void _launchURL() async {
+    if (await canLaunch(detall)) {
+      await launch(detall);
+    } else {
+      throw 'Could not launch $detall';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +27,13 @@ class InfoAbres extends StatelessWidget {
             Text('Nom: ${abre['nom']}'),
             Text('Varietat: ${abre['varietat']}'),
             Text('Tipus: ${abre['tipus']}'),
-            Text('Autocton: ${abre['autocton'] ? 'Yes' : 'No'}'),
+            Text('Autocton: ${abre['autocton'] ? 'Si' : 'No'}'),
             Image.network(abre['foto']),
             Text('Detall: ${abre['detall']}'),
+            // InkWell(
+            //   child: Text('Detall: $detall'),
+            //   onTap: _launchURL,
+            // ),
           ],
         ),
       ),
